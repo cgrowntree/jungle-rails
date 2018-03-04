eed798a6ad6cb964fd59c5123533fabddf9608ac
Christopher Rowntree <cgrowntree@gmail.com>
2018-03-04 09:06:21 +0000

    Added delete review and added styling for stars and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,14 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+ 
+    redirect_to @product
+  end
+
   private
   def review_params
     params.require(:review).permit(:rating, :description)
